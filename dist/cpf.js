bdacce43c1ebac4f3739b9c5d7dc6b510992be93
Nando Vieira <fnando.vieira@gmail.com>
2018-10-26 11:16:11 -0700

    Bump up version.

--- dist/cpf.js
+++ dist/cpf.js
@@ -96,6 +96,15 @@ var BLACKLIST = ["00000000000", "11111111111", "22222222222", "33333333333", "44
 var STRICT_STRIP_REGEX = /[.-]/g;
 var LOOSE_STRIP_REGEX = /[^\d]/g;
 
+/**
+ * Compute the Verifier Digit (or "Dígito Verificador (DV)" in PT-BR).
+ *
+ * You can learn more about the algorithm on [wikipedia (pt-br)](https://pt.wikipedia.org/wiki/D%C3%ADgito_verificador)
+ *
+ * @export
+ * @param {string} numbers a string with only numbers.
+ * @returns {number} the verifier digit.
+ */
 function verifierDigit(numbers) {
   numbers = numbers.split("").map(function (number) {
     return parseInt(number, 10);
@@ -114,17 +123,55 @@ function verifierDigit(numbers) {
   return mod < 2 ? 0 : 11 - mod;
 }
 
-function format(number) {
-  return strip(number).replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, "$1.$2.$3-$4");
+/**
+ * Transform the input into a pretty CPF format.
+ *
+ * Example:
+ * ```
+ * format('12345678901');
+ * // Result: '123.456.789-01'
+ * ```
+ *
+ * @export
+ * @param {string} cpf the CPF.
+ * @returns {string} the formatted CPF.
+ */
+function format(cpf) {
+  return strip(cpf).replace(/^(\d{3})(\d{3})(\d{3})(\d{2})$/, "$1.$2.$3-$4");
 }
 
-function strip(number, strict) {
-  var regex = strict ? STRICT_STRIP_REGEX : LOOSE_STRIP_REGEX;
-  return (number || "").toString().replace(regex, "");
+/**
+ * Remove some characters from the `number` input.
+ *
+ * Example:
+ * ```
+ * strip('29537995593'); // Result: '29537995593'
+ * strip('295.379.955-93'); // Result: '29537995593'
+ * strip('295a379b9c5d59e3'); // Result: '29537995593'
+ * strip('295a379b9c5d59e3', true); // Result: '295a379b9c5d59e3' - Atention!
+ * ```
+ *
+ * @export
+ * @param {string} cpf the CPF text.
+ * @param {boolean} [isStrict] if `true`, it will remove only `.` and `-` characters.
+ *                             Otherwise, it will remove all non-digit (`[^\d]`) characters. Optional.
+ * @returns {string} the stripped CPF.
+ */
+function strip(cpf, isStrict) {
+  var regex = isStrict ? STRICT_STRIP_REGEX : LOOSE_STRIP_REGEX;
+  return (cpf || "").toString().replace(regex, "");
 }
 
-function isValid(number, strict) {
-  var stripped = strip(number, strict);
+/**
+ * Validate the CPF.
+ *
+ * @export
+ * @param {string} cpf the CPF number.
+ * @param {boolean} [isStrict] if `true`, it will accept only `digits`, `.` and `-` characters. Optional.
+ * @returns {boolean} `true` if CPF is valid. Otherwise, `false`.
+ */
+function isValid(cpf, isStrict) {
+  var stripped = strip(cpf, isStrict);
 
   // CPF must be defined
   if (!stripped) {
@@ -148,7 +195,14 @@ function isValid(number, strict) {
   return numbers.substr(-2) === stripped.substr(-2);
 }
 
-function generate(formatted) {
+/**
+ * Generate a random CPF.
+ *
+ * @export
+ * @param {boolean} [useFormat] if `true`, it will format using `.` and `-`. Optional.
+ * @returns {string} the CPF.
+ */
+function generate(useFormat) {
   var numbers = "";
 
   for (var i = 0; i < 9; i += 1) {
@@ -158,7 +212,7 @@ function generate(formatted) {
   numbers += verifierDigit(numbers);
   numbers += verifierDigit(numbers);
 
-  return formatted ? format(numbers) : numbers;
+  return useFormat ? format(numbers) : numbers;
 }
 
 /***/ })
